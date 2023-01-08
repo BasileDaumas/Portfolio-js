0243bd3d16e4539ad9ae84cf9e4a11200eaf46d4
BasileDaumas <114342178+BasileDaumas@users.noreply.github.com>
2023-01-08 22:15:18 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -15,3 +15,66 @@ button.addEventListener("click", function() {
     button.innerText = "Dark Mode";
   }
 });
+
+
+
+"use strict";
+
+let carousel_slides = document.getElementsByClassName("carousel-slide");
+let number_of_slides = carousel_slides.length;
+
+let index = 0;
+
+let _navigation_dots = document.getElementById("navigation-dots");
+let dot = document.createElement("i");
+dot.classList.add("fa-solid", "fa-circle", "dot");
+let unique_dot;
+
+for (let x=1; x<number_of_slides; x++) {
+    unique_dot = dot.cloneNode();
+    unique_dot.addEventListener("click", function() {changeSlide('jump-to', x)});
+    _navigation_dots.appendChild(unique_dot);
+}
+
+let navigation_dots = _navigation_dots.children;
+
+for (let i=1; i<number_of_slides; i++) {
+    carousel_slides[i].style.display = "none";
+}
+
+function changeSlide(action, value) {
+
+    carousel_slides[index].style.display = "none";
+    navigation_dots[index].removeAttribute("id");
+    
+    switch (action) {
+        case "move-right": {
+
+            if (index === number_of_slides-1) {
+                index = 0;
+            }
+            else {
+                index += 1;
+            }
+            break;
+        }
+        
+        case "move-left": {
+            if (index === 0) {
+                index = number_of_slides-1;
+            }
+            else {
+                index -= 1;
+            }
+            break;
+        }
+        
+        case "jump-to": {
+            index = value;
+            break;
+        }
+    }
+            
+    carousel_slides[index].style.display = "flex";
+    navigation_dots[index].id = "active-dot";
+}
